9bce6dfcbfb042080174ce6d60f8bc3cbd190517
Daniel Kaiser <dk@mhger.de>
2017-04-10 15:18:04 +0200

    added hide article in navigation module article-list (mxByteCore)
    Signed-off-by: Daniel Kaiser <dk@mhger.de>

--- www/system/modules/mhgCore/dca/tl_article.php
+++ www/system/modules/mhgCore/dca/tl_article.php
@@ -11,4 +11,15 @@
 
 mhg\Dca::modifyPalettes( ',space', '', 'tl_article' );
 
+$GLOBALS['TL_DCA']['tl_article']['fields']['hide'] = array(
+    'label' => &$GLOBALS['TL_LANG']['tl_article']['hide'],
+    'exclude' => true,
+    'inputType' => 'checkbox',
+    'eval' => array( 'tl_class' => 'w50' ),
+    'sql' => "char(1) NOT NULL default ''"
+);
+
 $GLOBALS['TL_DCA']['tl_article']['fields']['published']['default'] = 1;
+
+
+
