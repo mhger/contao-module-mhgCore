e25345946b66018732bfaa6a903e94aee998a2bf
Pierre Gersöne <pg@mhger.de>
2017-11-29 01:58:51 +0100

    removed modified frontend modules > moved to [mhgElements]
    Signed-off-by: Pierre Gersöne <pg@mhger.de>

--- TL_ROOT/system/modules/mhgCore/dca/tl_article.php
+++ TL_ROOT/system/modules/mhgCore/dca/tl_article.php
@@ -12,19 +12,8 @@
 /**
  * alter DCA pallettes
  */
-mhg\Dca::modifyPalettes(',space', '', 'tl_article');
-mhg\Dca::modifyPalettes(',guests', ',hide,guests', 'tl_article');
+mhg\Dca::modifyPalettes(array(',keywords', ',author', ',space'), array('', ',author,keywords', ''), 'tl_article');
 
-/**
- * add DCA fields
- */
-mhg\Dca::addField('tl_article', 'hide', array(
-    'label' => &$GLOBALS['TL_LANG']['tl_article']['hide'],
-    'exclude' => true,
-    'inputType' => 'checkbox',
-    'eval' => array('tl_class' => 'w50'),
-    'sql' => "char(1) NOT NULL default ''"
-));
 /**
  * alter DCA fields
  */
