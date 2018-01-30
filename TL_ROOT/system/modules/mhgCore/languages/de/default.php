ed1ade7e8b47274b7e353bf8c106c1d7f2826554
Pierre Gersöne <pg@mhger.de>
2018-01-30 13:47:38 +0100

    added misc / universal language vars
    Signed-off-by: Pierre Gersöne <pg@mhger.de>

--- TL_ROOT/system/modules/mhgCore/languages/de/default.php
+++ TL_ROOT/system/modules/mhgCore/languages/de/default.php
@@ -14,6 +14,12 @@
  */
 $GLOBALS['TL_LANG']['MSC']['credits'] = 'Credits';
 $GLOBALS['TL_LANG']['MSC']['dashboard'] = 'Dashboard';
+
+$GLOBALS['TL_LANG']['MSC']['nextPage'] = 'Nächste Seite';
+$GLOBALS['TL_LANG']['MSC']['nextPageTitle'] = 'Weiter zur nächsten Seite';
+$GLOBALS['TL_LANG']['MSC']['prevPage'] = 'Vorherige Seite';
+$GLOBALS['TL_LANG']['MSC']['prevPageTitle'] = 'Zurück zur vorherigen Seite';
+
 $GLOBALS['TL_LANG']['MSC']['page'] = 'Seite';
 $GLOBALS['TL_LANG']['MSC']['medienhausGersoene'] = 'Medienhaus Gersöne';
 $GLOBALS['TL_LANG']['MSC']['mhgFooter'] = 'Ein Projekt der <a href="https://www.medienhaus-gersoene.de" target="_blank" title="Medienhaus Gersöne - Agentur für Neue Medien: Web, Design & Marketing">Medienhaus Gersöne</a>';
