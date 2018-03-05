ac611fa43ceabe82487e9363dc7d90b65f92cc18
Pierre Gersöne <pg@mhger.de>
2018-03-05 08:16:40 +0100

    added placeholder language var and placeholder styling
    Signed-off-by: Pierre Gersöne <pg@mhger.de>

--- TL_ROOT/system/modules/mhgCore/languages/de/default.php
+++ TL_ROOT/system/modules/mhgCore/languages/de/default.php
@@ -22,7 +22,7 @@ $GLOBALS['TL_LANG']['MSC']['nextPage'] = 'Nächste Seite';
 $GLOBALS['TL_LANG']['MSC']['nextPageTitle'] = 'Weiter zur nächsten Seite';
 $GLOBALS['TL_LANG']['MSC']['prevPage'] = 'Vorherige Seite';
 $GLOBALS['TL_LANG']['MSC']['prevPageTitle'] = 'Zurück zur vorherigen Seite';
-
+$GLOBALS['TL_LANG']['MSC']['optional'] = 'Optional';
 $GLOBALS['TL_LANG']['MSC']['page'] = 'Seite';
 $GLOBALS['TL_LANG']['MSC']['medienhausGersoene'] = 'Medienhaus Gersöne';
 $GLOBALS['TL_LANG']['MSC']['mhgFooterNote'] = 'Ein Projekt der <a href="https://www.medienhaus-gersoene.de" target="_blank" title="Medienhaus Gersöne - Agentur für Neue Medien: Web, Design & Marketing">Medienhaus Gersöne</a>';
